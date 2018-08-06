1a1d3a81aa6c6452ba6ddd3c4b9cc5aaa9e430af
FrancescoGalasso <francescogalasso@alfadispenser.com>
2018-08-06 12:16:45 +0200

    test json to template (2)

--- file_master_online/product/views.py
+++ file_master_online/product/views.py
@@ -11,29 +11,74 @@ def product_detail(request, pk):
     # print(_json)
     print(product)
     data =product.data
+    print("print data")
     print(data)
         # {u'office': {u'medical': [{u'use': u'reception', u'sq-ft': 50, u'price': 75, u'room-number': 100}, {u'use': u'waiting', u'sq-ft': 250, u'price': 75, u'room-number': 101}, {u'use': u'examination', u'sq-ft': 125, u'price': 150, u'room-number': 102}, {u'use': u'examination', u'sq-ft': 125, u'price': 150, u'room-number': 103}, {u'use': u'office', u'sq-ft': 150, u'price': 100, u'room-number': 104}]}, u'parking': {u'price': 750, u'style': u'covered', u'location': u'premium'}}
-    test = json.dumps(data)
-    print(test)
+    # test = json.dumps(data)
+    # print(test)
         # {"office": {"medical": [{"use": "reception", "sq-ft": 50, "price": 75, "room-number": 100}, {"use": "waiting", "sq-ft": 250, "price": 75, "room-number": 101}, {"use": "examination", "sq-ft": 125, "price": 150, "room-number": 102}, {"use": "examination", "sq-ft": 125, "price": 150, "room-number": 103}, {"use": "office", "sq-ft": 150, "price": 100, "room-number": 104}]}, "parking": {"price": 750, "style": "covered", "location": "premium"}}
 
-    test2 = json.loads(test)
+    # test2 = json.loads(test)
     # data  = json.loads(data)
     # print(_json['base_product'][0])
     prod_name = product.name
+    print("")
     print(prod_name)
-
     print("")
     # Create fixed data structures to pass to template
     # data could equally come from database queries
     # web services or social APIs
-    STORE_NAME = 'Downtown'
-    store_address = {'street':'Main #385','city':'San Diego','state':'CA'}
-    store_amenities = ['WiFi','A/C']
-    store_menu = ((0,''),(1,'Drinks'),(2,'Food'))
-    values_for_template = {'store_name':STORE_NAME, 'store_address':store_address, 'store_amenities':store_amenities, 'store_menu':store_menu}
+    PRODUCT_NAME = product.name
+    # store_address = {'street':'Main #385','city':'San Diego','state':'CA'}
+    # store_amenities = ['WiFi','A/C']
+    # store_menu = ((0,''),(1,'Drinks'),(2,'Food'))
+    try:
+        # {
+        #     "raw_material": "H20",
+        #     "base_product": [{
+        #         "g_100g": "x",
+        #         "base_name": "pastello"
+        #     }, {
+        #         "g_100g": "y",
+        #         "base_name": "base 2"
+        #     }],
+        #     "specific_weight": "1.999",
+        #     "RM_cost": "0.68541"
+        # }
+        # product_raw = product.data['raw_material']
+        # product_rmcost = product.data['RM_cost']
+        # product_bases = product.data['base_product']
+        # product_weight = product.data['specific_weight']
+        # product_bases = product.data['base_product']
+        # print("product_bases {}".format(product_bases))
+        # print(product_bases[0]['base_name'])
+        # print(product_bases[1]['base_name'])
+        # print(product_bases[1]['g_100g'])
+        # values_for_template = {'product_name':PRODUCT_NAME, 'product_raw':product_raw, 'product_rmcost':product_rmcost, 'product_weight':product_weight, 'product_bases':product_bases}
+        # print(values_for_template)
 
-    return render(request, 'product/product_detail.html', values_for_template)    
+        # ~~~
+        # {
+        #     "data": [{
+        #         "raw_material": "H20",
+        #         "specific_weight": "1.123",
+        #         "RM_cost": "0.07",
+        #         "bases": [{
+        #             "g_100g": "x",
+        #             "name": "pastel"
+        #         }, {
+        #             "g_100g": "y",
+        #             "name": "base2"
+        #         }]
+        #     }]
+        # }
+
+        lista = data['data']
+        print("lista")
+        print(lista)
+    except:
+        lista = { }
+    return render(request, 'product/product_detail.html', {'list': lista, 'prod_name': prod_name})    
     # return render(request, 'product/product_detail.html')
 
 
