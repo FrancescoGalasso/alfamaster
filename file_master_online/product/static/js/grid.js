3b9ca5e590a4437e4655a57dface57649320c038
FrancescoGalasso <francescogalasso@alfadispenser.com>
2018-09-20 08:55:28 +0200

    chore - grid.js

--- file_master_online/product/static/js/grid.js
+++ file_master_online/product/static/js/grid.js
@@ -99,19 +99,26 @@ function creationTHead(tr_head, list_of_thead){
 }
 
 function creationTBody(num_raw_material, tableBody){
+    var defaultRawMaterial = ['H<sub>2</sub>O', 'Binder', 'TiO<sub>2</sub>']
+
     for (var i = 0; i< num_raw_material; i ++){
         var tr_body = document.createElement('tr')
         tableBody.appendChild(tr_body)
 
         for (var j=0; j< td_counter; j++){
-        var td = document.createElement('TD')
-        // td.appendChild(document.createTextNode("Cell " + i + "," + j))
-        td.style.height = "30px"
-        if (j < 4 || j == 8){
-            td.contentEditable = true
-            td.style.backgroundColor = "#ffff00"
-        }
-        tr_body.appendChild(td)           
+            var td = document.createElement('TD')
+            // td.appendChild(document.createTextNode("Cell " + i + "," + j))
+            td.style.height = "30px"
+            if (j < 4 || j == 8){
+                td.contentEditable = true
+                td.style.backgroundColor = "#ffff00"
+            }
+            if (j == 0 && i < 3){
+                td.innerHTML = defaultRawMaterial[i]
+                td.contentEditable = false
+                td.style.backgroundColor = "transparent";
+            }
+            tr_body.appendChild(td)           
         }
     }
 }
