7177353b85891b82fe2e1f7182445edc24ba6678
FrancescoGalasso <francescogalasso@alfadispenser.com>
2018-10-12 14:55:12 +0200

    [style] - product/views.py
    removed unused commented print

--- file_master_online/product/views.py
+++ file_master_online/product/views.py
@@ -45,9 +45,7 @@ def product_new(request):
     print(request.POST)
     if request.method == "POST":
         my_name = request.POST.get('name')
-        # print(my_name)
         my_data = request.POST.get('data')
-        # print(my_data)
         d = json.loads(my_data)
         Product.objects.create(name=my_name, data=d)
     context={}
