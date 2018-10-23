0868ff4832f3c9b14eebf1c674831d220e166dbf
FrancescoGalasso <francescogalasso@alfadispenser.com>
2018-10-23 10:19:16 +0200

    [build] - model Product modified
    added attribute Revision. Added new migration

--- file_master_online/product/models.py
+++ file_master_online/product/models.py
@@ -14,12 +14,13 @@ class Product(models.Model):
     data = JSONField(default=list)
     created_date = models.DateTimeField(
             default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
+    # published_date = models.DateTimeField(
+    #         blank=True, null=True)
+    revision = models.IntegerField(default=1)
 
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
+    # def publish(self):
+    #     self.published_date = timezone.now()
+    #     self.save()
 
     def __str__(self):
         return self.name
