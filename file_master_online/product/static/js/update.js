c1e4389d8ed72b7e4b589f57bc2db3cfdcb50f80
FrancescoGalasso <francescogalasso@alfadispenser.com>
2018-12-13 13:06:27 +0100

    [style] - some clean up (2)

--- file_master_online/product/static/js/update.js
+++ file_master_online/product/static/js/update.js
@@ -41,7 +41,6 @@ $( document ).ready(function() {
             cells[i].innerHTML = ""
         }
 
-        // setClassesForCalculationUpdate()
         setClassesForCalculation()
 
         document.getElementById("updateCalculateBtn").disabled = false;
--- file_master_online/product/static/js/utils.js
+++ file_master_online/product/static/js/utils.js
@@ -9,7 +9,6 @@ $( document ).ready(function() {
     for(var i=0; i<3; i++){
         body.rows[i].cells[0].innerHTML = materials[i]
     }
-    // body.rows[0].cells[0].innerHTML = "test!"
 
     var table = document.getElementById('tdetail')
     var tableFoot = document.createElement('TFOOT')
@@ -19,14 +18,6 @@ $( document ).ready(function() {
     tableFoot.style.borderStyle = "solid" // css 
     tableFoot.style.borderColor = "black" // css
     creationTFoot(tableFoot, tr_foot)
-
-    // $(function($){
-    //     var foot = $("#tdetail").find('tfoot');
-    //     if (!foot.length) foot = $('<tfoot>').appendTo("#tdetail");
-    //     num_bases = document.getElementById('tdetail').rows[0].cells.length -3
-    //     foot.append($('<td colspan="3"><b>Total</b></td><td>a</td><td>b</td>'));
-    //     $('#tdetail tfoot tr:first').remove();
-    // })
 
     setClassesForCalculation()
 
