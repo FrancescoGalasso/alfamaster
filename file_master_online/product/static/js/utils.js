4889da6d8505adae5d8f56a9a7d5e73d09277aa8
FrancescoGalasso <francescogalasso@alfadispenser.com>
2018-12-18 14:28:09 +0100

    [style] - some clean up

--- file_master_online/product/static/js/utils.js
+++ file_master_online/product/static/js/utils.js
@@ -1,10 +1,10 @@
 $( document ).ready(function() {
 
     var body = document.getElementsByTagName("TBODY")[0]
-    console.log(body)
+    // console.log(body)
 
     var amountOfRows = $("#tdetail  tbody  tr").length
-    console.log(amountOfRows)
+    // console.log(amountOfRows)
     var materials = ['H<sub>2</sub>O', 'Binder', 'TiO<sub>2</sub>']
     for(var i=0; i<3; i++){
         body.rows[i].cells[0].innerHTML = materials[i]
