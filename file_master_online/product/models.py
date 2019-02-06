54c1bf774c4c9da63e5f6e123221da6dc03d1fd2
FrancescoGalasso <francescogalasso@alfadispenser.com>
2019-02-06 09:46:03 +0100

    [style] - clean up

--- file_master_online/product/models.py
+++ file_master_online/product/models.py
@@ -6,23 +6,11 @@ from django.contrib.postgres.fields import JSONField
 
 
 class Product(models.Model):
-    # name = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    # text = models.TextField()
-#     id=models.IntegerField(primary_key=True)
     name = models.CharField(max_length=25, blank=False)
     owner = models.CharField(max_length=25, default='')
-    # data = JSONField(default=list)
     created_date = models.DateTimeField(
             default=timezone.now)
-    # published_date = models.DateTimeField(
-    #         blank=True, null=True)
-    # revision = models.IntegerField(default=1)
     currencies = models.CharField(max_length=25, default='')
-
-
-    # def publish(self):
-    #     self.published_date = timezone.now()
-    #     self.save()
 
     def __str__(self):
         return self.name
