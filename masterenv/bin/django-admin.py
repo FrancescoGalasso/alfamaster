d53fa24ae84d81505b6606d83107f608d9bcebc9
FrancescoGalasso <francescogalasso@alfadispenser.com>
2019-04-26 10:50:24 +0200

    [build] - update struct project (1)

--- masterenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/galasso/Documents/sites/_master_online/alfamaster/masterenv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
